285f158d548f5e08d0ac7fec330fd60f19d18a09
Mazharul Islam <swopnil420@gmail.com>
2022-06-25 09:39:14 +0600

    removed unused component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import React from 'react';
 import { theme } from '../../theme';
 
